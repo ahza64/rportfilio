cb4e627208ff17a4f3f0832468af7f1a245f332f
ahza64 <ahza64@yahoo.com>
2016-09-01 18:22:25 -0700

    production fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = false
   config.assets.compile = false
+  config.serve_static_assets = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
